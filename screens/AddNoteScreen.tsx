f620ce58835dd2ae9b48d5a44053aced1bb54511
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-05 20:13:38 +0530

    add code of add not

---
+++ screens/AddNoteScreen.tsx
@@ -0,0 +1,11 @@
+// screens/AddNoteScreen.tsx
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function AddNoteScreen() {
+  return (
+    <View>
+      <Text>Add Note Screen</Text>
+    </View>
+  );
+}
