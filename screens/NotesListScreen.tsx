3bcca8d958e206a01edcf8939fa0fb8b74ebd697
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-05 20:12:09 +0530

    update bote list code

---
+++ screens/NotesListScreen.tsx
@@ -0,0 +1,11 @@
+// screens/NotesListScreen.tsx
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function NotesListScreen() {
+  return (
+    <View>
+      <Text>Notes List Screen</Text>
+    </View>
+  );
+}
