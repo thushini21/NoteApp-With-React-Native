6fc06328fe94dc38aa346b2b91b2923b2ffb9673
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-05 20:10:07 +0530

    update and create not list tsx

---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+// screens/LoginScreen.tsx
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
