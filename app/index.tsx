ab17fc949e18bfad47255144be6c64b4829f5b7f
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-14 15:47:14 +0530

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,21 @@
 import { useEffect, useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
 import { useRouter } from "expo-router";
-
+import { AuthContext } from "../context/AuthContext";
+import { View, Text, ActivityIndicator } from "react-native";
 
 export default function Index() {
   const { user } = useContext(AuthContext);
   const router = useRouter();
 
   useEffect(() => {
-    if(user) router.replace("../dashboard/notes");
-    else router.replace("../login");
+    if(user) router.replace("../notes");
+    else router.replace("/login");
   }, [user]);
 
-  return null;
+  return (
+    <View style={{ flex:1, justifyContent:"center", alignItems:"center" }}>
+      <ActivityIndicator size="large"/>
+      <Text>Loading...</Text>
+    </View>
+  );
 }
