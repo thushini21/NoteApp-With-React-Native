982799a573e71219cbbf0e41005c979b157d5fba
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-14 15:00:26 +0530

    create authService and ad logn and rejister functions to it

---
+++ services/authService.ts
@@ -0,0 +1,14 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from "../firebase";
+
+export const register = async (email: string, password: string) => {
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const login = async (email: string, password: string) => {
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const logout = async () => {
+  return await signOut(auth);
+};
