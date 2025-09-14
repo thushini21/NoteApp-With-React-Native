a441416077b3bda387d8c214b92eca8bc23f9fa7
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-14 13:06:59 +0530

    update code of index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useRouter } from "expo-router";
+
+export default function Index() {
+  const { user } = useContext(AuthContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    if(user) router.replace("/dashboard/notes");
+    else router.replace("/auth/login");
+  }, [user]);
+
+  return null;
+}
