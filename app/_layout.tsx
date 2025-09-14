412e9fe117d47781b533a33da10deee514273232
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-14 17:36:10 +0530

    importy Notecontext

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "../context/AuthContext";
+import { NotesProvider } from "../context/NotesContext";
 
 export default function RootLayout() {
   return (
