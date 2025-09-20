356b654e292c534e169adf28b5fba7b427f5b601
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-20 10:55:40 +0530

    update sttus bar info

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "../context/AuthContext";
 import { NotesProvider } from "../context/NotesContext";
+import StatusBarInfo from "./components/StatusBarInfo";
 
 export default function RootLayout() {
   return (
     <AuthProvider>
       <NotesProvider>
+        <StatusBarInfo />
         <Stack screenOptions={{ headerShown: false }} />
       </NotesProvider>
     </AuthProvider>
