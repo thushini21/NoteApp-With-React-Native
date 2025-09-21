c55b73d9f54fcd5f33b99ed9ad1f515fdfc8a12d
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-21 22:12:46 +0530

    add time provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,18 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "../context/AuthContext";
 import { NotesProvider } from "../context/NotesContext";
+import { ThemeProvider } from "../context/ThemeContext";
 import StatusBarInfo from "./components/StatusBarInfo";
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <NotesProvider>
-        <StatusBarInfo />
-        <Stack screenOptions={{ headerShown: false }} />
-      </NotesProvider>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <NotesProvider>
+          <StatusBarInfo />
+          <Stack screenOptions={{ headerShown: false }} />
+        </NotesProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
